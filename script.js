20f6d9c6e1665bf9a6946bb9f6c6cb46077b3c4a
LegoMyEgg0 <85911008+LegoMyEgg0@users.noreply.github.com>
2024-04-15 12:20:29 -0400

    fixed adding types each type should now be in its own div and added to the #types element.

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const speed = document.getElementById("speed");
 const pokeApi = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon";
 
 const getPokemon = async (pokemon) => {
-  console.log(pokemon);
+  //console.log(pokemon);
   try {
     const res = await fetch(pokeApi + "/" + pokemon);
     const whosThatPokemon = await res.json();
@@ -33,23 +33,23 @@ const getPokemon = async (pokemon) => {
 const addInfo = (pokemon) => {
   //console.log(pokemon);
   const { sprites, name, id, weight, height, types, stats } = pokemon;
-  console.log(name, id, weight, height, types, stats);
-  console.log(
-    "sprites",
-    sprites,
-    "hp",
-    stats[0].base_stat,
-    "attack",
-    stats[1].base_stat,
-    "defense",
-    stats[2].base_stat,
-    "special-attack",
-    stats[3].base_stat,
-    "special-defense",
-    stats[4].base_stat,
-    "speed",
-    stats[5].base_stat
-  );
+  //console.log(name, id, weight, height, types, stats);
+  // console.log(
+  //   "sprites",
+  //   sprites,
+  //   "hp",
+  //   stats[0].base_stat,
+  //   "attack",
+  //   stats[1].base_stat,
+  //   "defense",
+  //   stats[2].base_stat,
+  //   "special-attack",
+  //   stats[3].base_stat,
+  //   "special-defense",
+  //   stats[4].base_stat,
+  //   "speed",
+  //   stats[5].base_stat
+  // );
   picture.innerHTML = "";
   const image = document.createElement("img");
   image.alt = "pokemon image";
@@ -60,11 +60,11 @@ const addInfo = (pokemon) => {
   pokemonId.innerHTML = "#" + id;
   pokemonWeight.innerHTML = weight;
   pokemonHeight.innerHTML = height;
-
-  //const type = document.createElement("div");
-
-  pokemonTypes.innerHTML =
-    "types " + types.reduce((acc, val) => acc + " " + val.type.name, "");
+  pokemonTypes.innerHTML = "";
+  pokemonTypes.innerHTML = types.reduce(
+    (acc, val) => acc + `<div class="${val.type.name}">${val.type.name}</div>`,
+    ``
+  );
   hp.innerHTML = stats[0].base_stat;
   attack.innerHTML = stats[1].base_stat;
   defense.innerHTML = stats[2].base_stat;
@@ -74,9 +74,9 @@ const addInfo = (pokemon) => {
 };
 
 const pokemonIdAndNameCheck = () => {
-  console.log("nan?", Number.isNaN(+input.value), typeof input, input.value);
+  //console.log("nan?", Number.isNaN(+input.value), typeof input, input.value);
   if (Number.isNaN(+input.value)) {
-    console.log();
+    //console.log();
     //Note: Pokémon names should be in lowercase
     const lowercasePokemon = input.value.toLowerCase();
     //have special characters removed
@@ -86,7 +86,7 @@ const pokemonIdAndNameCheck = () => {
     lowercasePokemon.replaceAll(/\W/g, "-");
     //Also, if the Pokémon has either ♀ or ♂ as part of its name
     //the format is {name-f} or {name-m}, respectively
-    console.log("lowercase", lowercasePokemon);
+    //console.log("lowercase", lowercasePokemon);
     getPokemon(lowercasePokemon);
   } else {
     //look for id #
